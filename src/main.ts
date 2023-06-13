f3106233bc3a729ba97aeb2f3c70b5ed178b53f2
Locria Cyber <74560659+iacore@users.noreply.github.com>
2023-06-13 19:50:18 +0000

    Add physics settings (gui broken)

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,22 @@ import * as dat from "dat.gui"
 const gui = new dat.GUI()
 
 const options = {
-  arrowLength: 4,
+  arrowSize: 4,
   arrowRelPos: 0.5,
 }
 
+const physicsSettings = {
+  // defaults from https://github.com/anvaka/ngraph.physics.simulator/blob/master/index.js
+  timeStep: 20,
+  gravity: -1.2,
+  theta: 0.8,
+  springLength: 10,
+  springCoefficient: 0.0008,
+  dragCoefficient: 0.02
+}
+
 const myGraph = ForceGraph3D()
+
 myGraph(appel)
   .graphData(data)
   .nodeAutoColorBy("group")
@@ -25,16 +36,25 @@ myGraph(appel)
     sprite.backgroundColor = "#00000055" // transparent black
     sprite.borderColor = node.color
     sprite.borderWidth = 0.5
-    sprite.textHeight = 8
+    sprite.textHeight = 4
     return sprite
   })
-  .linkDirectionalArrowLength(options.arrowLength)
+  .forceEngine("ngraph")
+  .ngraphPhysics(physicsSettings)
+  .linkDirectionalArrowLength(options.arrowSize)
   .linkDirectionalArrowRelPos(options.arrowRelPos)
 // .linkDirectionalArrowColor("#ffffff")
 
 gui
-  .add(options, "arrowLength", 0, 10)
+  .add(options, "arrowSize", 0, 10)
   .onChange((value: number) => myGraph.linkDirectionalArrowLength(value))
 gui
   .add(options, "arrowRelPos", 0, 1)
   .onChange((value: number) => myGraph.linkDirectionalArrowRelPos(value))
+
+const guiPhysics = gui.addFolder("Physics Settings")
+for (const key in physicsSettings) {
+  guiPhysics
+    .add(physicsSettings, key)
+    .onChange(() => myGraph.ngraphPhysics(physicsSettings))
+}
